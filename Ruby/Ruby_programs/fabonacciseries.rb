7c20cef44f609bf309b52cb4a38dcc8a5990ed6d
beryl systems <you@example.com>
2021-06-16 12:43:16 +0530

    first program of ruby fabonacci series

---
+++ Ruby/Ruby_programs/fabonacciseries.rb
@@ -0,0 +1,17 @@
+def Fabonacci_series(n_terms)
+    n1,n2=0,1
+    count =0
+    n_terms=n_terms.to_i
+    puts "Fabonacci Sequence"
+    while count < n_terms do
+        print("\n #{n1}")
+        nth = n1 + n2
+        n1=n2
+        n2=nth
+        count +=1
+    end
+end
+
+puts "How long u want the series to be"
+n_terms= gets 
+Fabonacci_series n_terms
