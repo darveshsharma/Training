5c8d18ed2bfe9116650666dac0c08aedb1885ea4
beryl systems <you@example.com>
2021-06-16 17:52:07 +0530

    function that maps files to their extension names

---
+++ Ruby/Ruby_programs/extension.rb
@@ -0,0 +1,12 @@
+def extension(array)
+    n_array=[]
+    array.each do |i|
+        x,y,z=i.partition(".")
+        n_array.push(z)
+    end
+    print n_array
+    puts
+end
+extension(["code.html", "code.css"])
+extension(["project1.jpg", "project1.pdf", "project1.mp3"])
+extension(["ruby.rb", "cplusplus.cpp", "python.py", "javascript.js"])
