3c6d04fd1884c6748ef12df08209daaca70d9321
beryl systems <you@example.com>
2021-06-16 15:37:02 +0530

    edabit programs easy one's

---
+++ Ruby/Ruby_programs/footballpoints.rb
@@ -0,0 +1,8 @@
+def football_points(win,draw,loose)
+    print (3 * win) +draw
+end
+
+win=gets.to_i
+draw=gets.to_i
+loose=gets.to_i
+football_points win,draw,loose
---
+++ Ruby/Ruby_programs/hackerspark.rb
@@ -0,0 +1,20 @@
+def hacker_speak(code)
+    code.each_char do |i|
+        if i=="a"
+            print '4'
+        elsif i=='e'
+            print '3'
+        elsif i=='i'
+            print '1'
+        elsif i=='o'
+            print '0'
+        elsif i=='s'
+            print '5'
+        else
+            print i
+        end
+    end
+    puts 
+end
+
+hacker_speak "hacker rank"
