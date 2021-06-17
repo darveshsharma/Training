ef5e62ad983fe2c00a493e775140ecb52e190350
beryl systems <you@example.com>
2021-06-17 15:42:53 +0530

    spoonerism program added

---
+++ Ruby/Ruby_programs/Spoonerisms.rb
@@ -0,0 +1,16 @@
+def spoonerise(str)
+    x=""
+    y=[]
+    a,b=str.split(" ")
+    a=a.chars
+    b=b.chars
+    x=a[0]
+    a[0]=b[0]
+    b[0]=x
+    y=y.push(a.join,b.join)
+    puts y.join(" ")
+end
+spoonerise("history lecture")
+spoonerise("loud noises")
+spoonerise("chow mein")
+spoonerise("edabit rules!")
