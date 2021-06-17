2d314e093fa1ad7ff6fa3675fa90981891014729
beryl systems <you@example.com>
2021-06-17 11:07:43 +0530

    flatten list program corrected

--- Ruby/Ruby_programs/flattenalist.rb
+++ Ruby/Ruby_programs/flattenalist.rb
@@ -1,13 +1,15 @@
+$a=Array.new
 def flatten_a_list(list)
-    a=[]
     list.each do |i|
         if i.class == list.class
             flatten_a_list(i)
         else
-            a.push(i)
+            $a.push(i)
         end
     end
-    print a
+    return $a
 end
 
-flatten_a_list [1, 2, [3, [4, 5], 6], 7, 8]
+x=[]
+x=flatten_a_list [1, 2, [3, 4, 5], [6, 7, 8]]
+print x
