8fb958f08c6e504cf69c6b8b3ea10d2f52a9c21e
beryl systems <you@example.com>
2021-06-21 18:26:08 +0530

    class_variables program added for class undertanding and types object understanding

---
+++ Ruby/Ruby_programs/class_and_variables.rb
@@ -0,0 +1,30 @@
+class Books
+    @@book_list=[]
+    @@author_title=[]
+    @@bookno=0
+    def getTitle(title)
+        @title=title
+        puts "Title: #{@title}"
+        @@author_title[0]=@title
+    end
+    def getAuthor(author)
+        @author = author
+        puts "Author: #{@author}"
+        @@author_title[1]=@author
+    end
+    def print_details()
+        @@book_list[@@bookno]=@@author_title
+        puts "Bookno:#{@@bookno},Title:#{@@book_list[@@bookno][0]},Author:#{@@book_list[@@bookno][1]}"
+        @@bookno=@@bookno+1
+    end
+end
+
+book1=Books.new
+book2=Books.new
+
+book1.getTitle("Harry Porter")
+book1.getAuthor("JK Rowling")
+book1.print_details()
+book2.getTitle("War and Peace")
+book2.getAuthor("Leo Tolstoy")
+book2.print_details()
