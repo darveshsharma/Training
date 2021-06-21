d5c1777577b3d0fc1c777250c31bad89a5da8baa
beryl systems <you@example.com>
2021-06-21 16:30:56 +0530

    exceptioon handling program added

---
+++ Ruby/Ruby_programs/exception.rb
@@ -0,0 +1,34 @@
+
+begin 
+    file=open("not exist","r")
+    if file 
+        puts "file opened successfully"
+    end
+rescue
+    file =STDIN
+end
+print file,"==",STDIN, "\n"
+
+
+fname="not exist"
+begin
+    file = open(fname,"r")
+    if file
+       puts "File opened successfully"
+    end
+ rescue
+    puts "file does not exist"
+    fname="temp.txt"
+    retry
+ end
+
+
+
+ begin
+    file=open("not exist","r")
+ rescue Exception => e
+    puts e.message
+    puts e.backtrace.inspect
+ ensure
+    puts "Ensuring execution"
+ end
