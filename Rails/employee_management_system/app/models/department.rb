94868033ad3de1cbd2e27be77ff1e2d934b82283
beryl systems <you@example.com>
2021-06-30 18:25:52 +0530

    added different types of validations in different models

--- Rails/employee_management_system/app/models/department.rb
+++ Rails/employee_management_system/app/models/department.rb
@@ -1,3 +1,4 @@
 class Department < ApplicationRecord
     has_many :employees
+    validates! :Department_Name, uniqueness: { case_sensitive: false }
 end
--- Rails/employee_management_system/app/models/employee.rb
+++ Rails/employee_management_system/app/models/employee.rb
@@ -3,4 +3,6 @@ class Employee < ApplicationRecord
     has_many :project_assignments
     has_many :projects, through: :project_assignments
     has_many :comments, as: :feedback
+    validates! :Name, presence: {message: "please enter"}
+    validates! :Gender, inclusion: {in: %w(M F m f other Other) , message: "%{value} can be M/F/Other!!"}
 end
